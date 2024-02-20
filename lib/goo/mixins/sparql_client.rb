0a30e18a5d0ed52e47382df7a2483581a68bcec8
Syphax bouazzouni <gs_bouazzouni@esi.dz>
2024-02-20 10:43:36 +0100

    Feature: update rdf gem to latest version  (#56)
    * un pin rdf version, to use the latest and add rdf vocab and xml
    
    * update URI class monkey patch because Addressable does no more exist
    
    * RDF::SKOS is replaced with RDF::Vocab::SKOS in the latest version of RDF
    
    * pin rdf version to 3.2.11 the latest version that support ruby 2.7
    
    * monkey path Literal::DateTime format to be supported by 4store
    
    * remove  addressable dependency

--- lib/goo/mixins/sparql_client.rb
+++ lib/goo/mixins/sparql_client.rb
@@ -6,7 +6,24 @@ end
 
 module RDF
   def self.URI(*args, &block)
-    return args.first
+    return RDF::URI.new(*args)
+  end
+
+  class URI
+    # Delegate any undefined method calls to the String object
+    def method_missing(method, *args, &block)
+      if self.to_s.respond_to?(method)
+        self.to_s.send(method, *args, &block)
+      else
+        super
+      end
+    end
+
+    # Ensure respond_to? reflects the delegated methods
+    def respond_to_missing?(method, include_private = false)
+      self.to_s.respond_to?(method) || super
+    end
+
   end
 
   class Writer
@@ -31,66 +48,10 @@ module RDF
     end
   end
 
-  class URI
-    def initialize(uri_or_options)
-      case uri_or_options
-        when Hash
-          @uri = Addressable::URI.new(uri_or_options)
-        when Addressable::URI
-          @uri = uri_or_options
-        else
-          @uri = uri_or_options.to_s
-          #@uri = Addressable::URI.parse(uri_or_options.to_s)
-      end
-    rescue Addressable::URI::InvalidURIError => e
-      raise ArgumentError, e.message
-    end
-
-    def method_missing(symbol, *args, &block)
-      unless @uri.respond_to?(symbol)
-        if (Addressable::URI.instance_methods.include?(symbol) && @uri.instance_of?(String))
-          @uri = Addressable::URI.parse(@uri)
-        end
-      end
-      if @uri.respond_to?(symbol)
-        case result = @uri.send(symbol, *args, &block)
-          when Addressable::URI
-            self.class.new(result)
-          else result
-        end
-      else
-        super
-      end
-    end
-
-    def last_part
-      f = fragment
-      return f if f
-      return to_s.split("/")[-1]
-    end
-
-    def respond_to?(symbol,include_private = false)
-      @uri.respond_to?(symbol,include_private=false) || super
-    end
-
-    def hash
-      @uri.to_s.hash
-    end
-
-  end #end URI
 
   class Literal
-    @@subclasses_by_uri = {}
-    def self.datatyped_class(uri)
-      return nil if uri.nil?
-      if @@subclasses.length != (@@subclasses_by_uri.length + 1)
-       @@subclasses.each do |child|
-        if child.const_defined?(:DATATYPE)
-          @@subclasses_by_uri[child.const_get(:DATATYPE).to_s] = child
-        end
-       end
-      end
-      return @@subclasses_by_uri[uri]
+    class DateTime < Temporal
+      FORMAT = '%Y-%m-%dT%H:%M:%S'.freeze # the format that is supported by 4store
     end
   end
 end #end RDF
